8cd0d0fe3405f64d50b60bb32d6bc40ffd922cb8
Javier <alubla1033@ieselcaminas.org>
2024-01-11 16:56:13 +0100

    daos_finalizados

---
+++ src/main/java/com/example/producto2/model/entity/Usuario.java
@@ -0,0 +1,111 @@
+package com.example.producto2.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name= "Usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false, unique = true)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "menu_has_usuario",
+            joinColumns = {@JoinColumn(name = "id_usuario")},
+            inverseJoinColumns = {@JoinColumn(name = "id_menu")}
+    )
+    private List<Menu> menus;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido")
+    Pedido pedido;
+
+    public Usuario(String nombre, String email, String username, String password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Usuario() {
+    }
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "users_roles",
+            joinColumns = {
+                    @JoinColumn(name = "user_id", referencedColumnName =
+                            "id_usuario",
+                            nullable = false, updatable = false)},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "role_id", referencedColumnName = "id",
+                            nullable = false, updatable = false)})
+    private Set<Role> rolesAssociated = new HashSet<>();
+
+    public Set<Role> getRolesAssociated() {
+        return rolesAssociated;
+    }
+
+    public void setRolesAssociated(Set<Role> rolesAssociated) {
+        this.rolesAssociated = rolesAssociated;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+
