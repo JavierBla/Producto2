dc1036ec2d325b4043df56dc248841aec1b0b0e7
Javier <alubla1033@ieselcaminas.org>
2024-01-17 18:39:11 +0100

    vista-controller_17-1-24

--- src/main/java/com/example/producto2/controller/ProductController.java
+++ src/main/java/com/example/producto2/controller/ProductController.java
@@ -2,13 +2,12 @@ package com.example.producto2.controller;
 
 
 import com.example.producto2.model.dao.ProductoDaoImpl;
+import com.example.producto2.model.entity.Menu;
 import com.example.producto2.model.entity.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -30,8 +29,36 @@ public class ProductController {
         return "create_product";
     }
 
+    @GetMapping("/product/{id}")
+    public String getMenuDetail(Model model, @PathVariable Integer id) {
+        Producto producto = ProductService.findByID(id);
+        if(producto != null) {
+            model.addAttribute("product", producto);
+            model.addAttribute("currentPage", "product");
+        }
+        return "edit_product";
+    }
+
+    @PostMapping("/product/update/{id}")
+    public String updateProduct(@PathVariable("id") Integer id,
+                                @ModelAttribute("product") Producto productDetails) {
+        Producto producto = ProductService.findByID(id);
+        if (producto != null) {
+            producto.setNombre(productDetails.getNombre());
+            producto.setPrecio(productDetails.getPrecio());
+            ProductService.update(producto);
+        }
+        return "redirect:/product/{id}";
+    }
+
+    @DeleteMapping("/product/delete/{id}")
+    public String deleteProduct(Model model, @ModelAttribute("product") Producto producto) {
+        ProductService.delete(producto.getId());
+        return "redirect:/product";
+    }
+
     @PostMapping("/product/save")
-    public String newMenu(Model model, @ModelAttribute("product") Producto product) {
+    public String newProduct(Model model, @ModelAttribute("product") Producto product) {
         ProductService.save(product);
         return "redirect:/product";
     }
